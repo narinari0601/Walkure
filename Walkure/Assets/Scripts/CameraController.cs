e934f90deb1b55bee65f093ec82f52277ae15c80
Narinari0601 <kouta19960601.kizuna-reo.t-mk@ezweb.ne.jp>
2020-09-14 15:20:33 +0900

    playerの移動 カメラがplayerを追う

---
+++ Walkure/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField, Header("メインカメラ")]
+    private GameObject m_Camera = null;
+
+    [SerializeField, Header("プレイヤー")]
+    private GameObject player = null;
+
+    [SerializeField, Header("プレイヤーとメインカメラの距離")]
+    private Vector3 offset = Vector3.zero;
+
+    private Vector3 mInitPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mInitPos = player.transform.position + offset;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Camera.transform.position = new Vector3(mInitPos.x, mInitPos.y, player.transform.position.z + offset.z);
+    }
+}
--- Walkure/Assets/Scripts/PlayerController.cs
+++ Walkure/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Rigidbody rb;
+
+    [SerializeField, Header("移動速度")]
+    private float speed = 1.0f;
+
+    private Vector3 velocity;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        velocity = Vector3.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+        //キーボード入力
+        float key_x = Input.GetAxisRaw("Horizontal");
+        float key_z = Input.GetAxisRaw("Vertical");
+
+        velocity.Set(key_x, 0, key_z);
+        velocity = velocity.normalized * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + velocity);
     }
 }
