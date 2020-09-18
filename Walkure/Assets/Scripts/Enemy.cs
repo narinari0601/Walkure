529a140b5ecb70c620fc91e204156a2e959f8985
Narinari0601 <kouta19960601.kizuna-reo.t-mk@ezweb.ne.jp>
2020-09-18 17:28:51 +0900

    画面内に入るとプレイヤーを追いかける敵追加

--- Walkure/Assets/Scripts/Enemy.cs
+++ Walkure/Assets/Scripts/Enemy.cs
@@ -44,4 +44,6 @@ public class Enemy : MonoBehaviour
     {
         currentHP -= damage;
     }
+
+  
 }
---
+++ Walkure/Assets/Scripts/EnemyChased.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChased : MonoBehaviour
+{
+    [SerializeField,Header("移動速度")]
+    private float speed = 0.5f;
+
+    private GameObject player;
+
+    private Vector3 vector;
+
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+
+        vector = Vector3.zero;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnWillRenderObject()
+    {
+        if (Camera.current.name == "MainCamera")
+        {
+            Chase();
+        }
+    }
+
+    private void Chase()
+    {
+        vector = (player.transform.position - transform.position).normalized;
+
+        transform.position += vector * speed * 0.01f;
+    }
+}
